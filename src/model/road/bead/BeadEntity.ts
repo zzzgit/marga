08dc099910b7974eb36df342daf9a7952c04faad
jonkee <zzzgoo@gmail.com>
2023-07-01 16:48:17 +0800

    feat: add methods to check pairs

--- src/model/road/bead/BeadEntity.ts
+++ src/model/road/bead/BeadEntity.ts
@@ -1,9 +1,31 @@
+import BancoPairBadge from "../../badge/BancoPairBadge"
+import PuntoPairBadge from "../../badge/PuntoPairBadge"
 import Entity from "../../entity/Entity"
 
 abstract class BeadEntity extends Entity {
 	abstract isTie(): boolean
 	abstract isBancoWon(): boolean
 	abstract isPuntoWon(): boolean
+
+	hasAnyPair(): boolean {
+		return this.hasBancoPair() || this.hasPuntoPair()
+	}
+
+	hasBothPair(): boolean {
+		return this.hasBancoPair() && this.hasPuntoPair()
+	}
+
+	hasBancoPair(): boolean {
+		return this.getTagArray().some((tag) => {
+			return tag instanceof BancoPairBadge
+		})
+	}
+
+	hasPuntoPair(): boolean {
+		return this.getTagArray().some((tag) => {
+			return tag instanceof PuntoPairBadge
+		})
+	}
 }
 
 export default BeadEntity
